bcc42306849425d8fba898f11a558b7365c1f7dc
Jacob Dionne <jd6378@mcla.edu>
2019-02-01 06:50:51 +0000

    completed code

--- server.js
+++ server.js
@@ -25,10 +25,26 @@ function requestHandler(req, res)
     }
     if (query['cmd'] == 'dotted')
     {
-        
+        var output;
+        output = query['word1'];
+        for(var i = 0; i < (30 - query['word1'].length - query['word2'].length);i++)
+            output += ".";
+        res.write(output + query['word2'])
     }
     if (query['cmd'] == 'stats')
     {
+        var sum = 0;
+        var max = 0;
+        var min = 100;
+        for (var i in query['grades'])
+            {
+                if(max < parseFloat(query['grades'][i]))
+                    max = parseFloat(query['grades'][i]);
+                if(min > parseFloat(query['grades'][i]))
+                    min = parseFloat(query['grades'][i]);
+                sum += parseFloat(query['grades'][i]);
+            }
+        res.write("Ave: " + sum/query['grades'].length + " Min: " + min + " Max: " + max)
         
     }
     res.end('');
